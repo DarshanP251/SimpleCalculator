9bb7f03c925eff440d379611c825939724fce6cb
DarshanP251 <darshanpatil0511@gmail.com>
2025-04-29 11:04:07 +0530

    saving to master

--- SimpleCalculator.java
+++ SimpleCalculator.java
@@ -1,4 +1,5 @@
 package Calcu;
+
 public class SimpleCalculator {
     public static void main(String[] args) {
         double num1 = 25;
@@ -7,6 +8,12 @@ public class SimpleCalculator {
         double sum = num1 + num2;
         double difference = num1 - num2;
         double product = num1 * num2;
+        double base = 4;
+        double exponent = 3;
+
+        double power = Math.pow(base, exponent);
+        double squareRoot = Math.sqrt(base);
+        double cubeRoot = Math.cbrt(base);
 
         System.out.println("Addition: " + sum);
         System.out.println("Subtraction: " + difference);
@@ -16,6 +23,11 @@ public class SimpleCalculator {
             System.out.println("Division: " + (num1 / num2));
         } else {
             System.out.println("Division: Cannot divide by zero.");
+
         }
+
+        System.out.println("Power (base^exponent): " + power);
+        System.out.println("Square Root of base: " + squareRoot);
+        System.out.println("Cube Root of base: " + cubeRoot);
     }
 }
